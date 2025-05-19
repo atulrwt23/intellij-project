1fed788170f9a357673c729bc4c5fb552a5879fc
usujoy <183721610+usujoy@users.noreply.github.com>
2025-05-19 10:29:43 +0530

    adds first test

---
+++ test/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void firstTest() {
+        int expected = 1;
+
+        assertEquals(expected, 1);
+    }
+}
