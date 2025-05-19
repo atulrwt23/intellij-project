73ff99164e24e21b59f307b8a06bc38ff2f49770
usujoy <183721610+usujoy@users.noreply.github.com>
2025-05-19 11:17:53 +0530

    Atul/Sujoy removes field parameter from  square class

--- src/models/Square.java
+++ src/models/Square.java
@@ -1,10 +1,8 @@
 package models;
 
 public class Square extends Rectangle {
-    private final int side;
 
     public Square(int side) {
         super(side, side);
-        this.side = side;
     }
 }
